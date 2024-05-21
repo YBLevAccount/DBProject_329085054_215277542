97bb8b1138c1dbf16cf89abbd122df09c72a4fd3
YBLevAccount <126462356+YBLevAccount@users.noreply.github.com>
2024-05-21 19:03:12 +0300

    Create selectAll.sql
    create show all data file

---
+++ level1/selectAll.sql
@@ -0,0 +1,10 @@
+-- Select all records from each table
+
+SELECT * FROM Person;
+SELECT * FROM Building;
+SELECT * FROM Room;
+SELECT * FROM Worker;
+SELECT * FROM Student;
+SELECT * FROM Manager;
+SELECT * FROM Cleaner;
+SELECT * FROM WorksIn;
